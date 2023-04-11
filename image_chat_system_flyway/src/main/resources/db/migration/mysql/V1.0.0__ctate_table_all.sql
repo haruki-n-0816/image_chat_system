4925c505ee75521b740d1603585c29fec2bfa223
haruki-n-0816 <kaihatsu.haruki.nagasawa@gmail.com>
2023-04-11 14:51:28 +0900

    リファクタリング

--- image_chat_system_flyway/src/main/resources/db/migration/mysql/V1.0.0__ctate_table_all.sql
+++ image_chat_system_flyway/src/main/resources/db/migration/mysql/V1.0.0__ctate_table_all.sql
@@ -1,14 +1,14 @@
 CREATE TABLE chat_room_list (
-	room_id INT NOT NULL AUTO_INCREMENT    	COMMENT 'id',
-	room_name CHAR(255)					COMMENT '名前',
+	room_id INT NOT NULL AUTO_INCREMENT    	COMMENT '部屋id',
+	room_name CHAR(255)					    COMMENT '名前',
 	PRIMARY KEY (room_id)
 );
 
 CREATE TABLE user_list (
-    id INT NOT NULL AUTO_INCREMENT,
-    user_id CHAR(10) NOT NULL,
-    user_name CHAR(255) NOT NULL,
-    user_mail CHAR(255) NOT NULL,
-    user_pw CHAR(255) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT          COMMENT '一意のid',
+    user_id CHAR(10) NOT NULL               COMMENT '英数字のid',
+    user_name CHAR(255) NOT NULL            COMMENT 'ユーザーの名前',
+    user_mail CHAR(255) NOT NULL            COMMENT 'メールアドレス',
+    user_pw CHAR(255) NOT NULL              COMMENT 'パスワード',
     PRIMARY KEY (id)
 );
