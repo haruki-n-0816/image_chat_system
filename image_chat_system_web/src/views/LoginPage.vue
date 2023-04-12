9adefb410592744a919a960034a87e22cbe19ef9
haruki-n-0816 <kaihatsu.haruki.nagasawa@gmail.com>
2023-04-12 16:54:20 +0900

    LoginPage.vue修正

--- image_chat_system_web/src/views/LoginPage.vue
+++ image_chat_system_web/src/views/LoginPage.vue
@@ -1,36 +1,32 @@
 <template>
-<!-- html -->
-<div>
-
-    <p>次郎チャットログイン</p><br>
-    <label>mail</label>
-    <input type="email" v-model="mail"><br>
-    <label>パスワード</label>
-    <input type="password" v-model="password" ><br>
-    <b-button variant="primary" onclick="alert('クリックされました')">ログイン</b-button><br>
-    <router-link to="後から入力">新規作成はこちらから</router-link>
-
-</div>
-
+    <div>
+        <p>次郎チャットログイン</p><br>
+        <label>mail</label>
+        <input type="email" v-model="mail"><br>
+        <label>パスワード</label>
+        <input type="password" v-model="password"><br>
+        <b-button variant="primary" @click="LoginCheck()">ログイン</b-button>
+        <br>
+        <router-link to="後から入力">新規作成はこちらから</router-link>
+    </div>
 </template>
 
 <script>
 import axios from 'axios';
 
-
-export default{
-    data(){
-        return{
-            mail:'',
-            password:''
+export default {
+    data() {
+        return {
+            mail: '',
+            password: ''
         }
     },
-    methods:{
-        async LoginCheck(){
+    methods: {
+        async LoginCheck() {
             try {
-                const respones = await axios.post('localhost:8080/login' ,{
-                mail:this.mail,
-                password:this.password,
+                const respones = await axios.post('localhost:8080/login', {
+                    mail: this.mail,
+                    password: this.password,
                 });
                 console.log(respones);
             } catch (error) {
