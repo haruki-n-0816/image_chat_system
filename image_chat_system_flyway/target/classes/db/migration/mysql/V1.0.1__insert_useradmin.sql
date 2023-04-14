677358c27dc3baa036f46c654b91aa0227e37b5b
haruki-n-0816 <kaihatsu.haruki.nagasawa@gmail.com>
2023-04-14 12:42:32 +0900

    ログインページ通信完成

--- image_chat_system_flyway/target/classes/db/migration/mysql/V1.0.1__insert_useradmin.sql
+++ image_chat_system_flyway/target/classes/db/migration/mysql/V1.0.1__insert_useradmin.sql
@@ -1,2 +1,2 @@
-INSERT INTO user_list (user_id, user_name, user_mail, user_pw) VALUES
+INSERT INTO user_list (user_id, user_name, user_mail, user_password) VALUES
 ('admin12345','admin','admin@mail.com','adminpw');
